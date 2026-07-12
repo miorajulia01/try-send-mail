61909995a4ce9c80ba2c43c8d1bb99673b3b45a6
miorajulia01 <miorahanitra01.com>
2026-07-12 21:03:41 +0300

    feat: add service and controller logic

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,8 +1,7 @@
 package com.example.demo.endpoint.rest.controller;
 
-import com.example.demo.mail.Email;
-import com.example.demo.mail.Mailer;
-import jakarta.mail.internet.InternetAddress;
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,15 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final Mailer mailer;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
-    @GetMapping("/hello")
-    @SneakyThrows
-    public String helloWorld(@RequestParam String to) {
-        var email =
-                new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
-
-        mailer.accept(email);
-        return "... world!";
-    }
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "... world!";
+  }
 }
